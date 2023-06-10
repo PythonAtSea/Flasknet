f2483eca0918488baded2a93a1075b12c5ad5a80
Hudson Green <hudsongreen27@gmail.com>
2023-06-10 13:17:13 -0500

    Databases Baby!

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, flash, redirect
 from config import Config
 from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 posts=[
     {"authorname": "Fred", "body" :"Hi buds"},
     {"authorname": "Sally", "body" :"why"},
 ]
-
+import models
 @app.route("/index")
 @app.route("/")
 def index():

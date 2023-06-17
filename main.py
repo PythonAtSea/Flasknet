e71f112c263658f22a9c07e12fa420d75ae95b48
Hudson Green <hudsongreen27@gmail.com>
2023-06-17 16:54:26 -0500

    Pagination

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def index():
 @app.route("/feed", methods=["POST","GET"])
 @login_required
 def feed():
+    page = request.args.get("page", 1, type=int)
     form = PostForm()
     if form.validate_on_submit():
         post = Post(body=form.post.data, author=current_user)
@@ -56,8 +57,19 @@ def feed():
         db.session.commit()
         flash("Your post is now live!")
         return redirect(url_for("feed"))
-    posts=current_user.followed_posts()
-    return render_template("feed.html", posts=posts, form=form)
+    posts=current_user.followed_posts().paginate(page=page, per_page=app.config["POSTS_PER_PAGE"])
+    next_url = url_for('feed', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('feed', page=posts.prev_num) if posts.has_prev else None
+    return render_template("feed.html", posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
+
+@app.route("/explore")
+@login_required
+def explore():
+    page = request.args.get("page", 1, type=int)
+    posts=Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config["POSTS_PER_PAGE"])
+    next_url = url_for('feed', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('feed', page=posts.prev_num) if posts.has_prev else None
+    return render_template("feed.html", posts=posts.items, next_url=next_url, prev_url=prev_url)
 @app.route("/login", methods=["GET","POST"])
 def login():
     if current_user.is_authenticated:
@@ -92,12 +104,12 @@ def signup():
 @app.route("/user/<username>")
 def user(username):
     form=EmptyForm()
+    page = request.args.get("page", 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {"author": user, "body": "test #1"},
-        {"author": user, "body": "test #2"},
-    ]
-    return render_template("user.html", user=user, posts=posts, form=form)
+    posts = user.post.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config["POSTS_PER_PAGE"])
+    next_url = url_for('user', username=username,page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=username, page=posts.prev_num) if posts.has_prev else None
+    return render_template("user.html", user=user, posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 @app.route("/edit", methods=["GET","POST"])
 @login_required
@@ -151,11 +163,7 @@ def unfollow(username):
     else:
         return redirect(url_for("feed"))
 
-@app.route("/explore")
-@login_required
-def explore():
-    posts=Post.query.all()
-    return render_template("feed.html", posts=posts)
+
 
 @app.before_request
 def before_request():

09fb2a3c846e011b75ab4876a13fb876aa2f1888
Hudson Green <hudsongreen27@gmail.com>
2023-06-17 20:14:10 -0500

    Slight change to paging

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ["pythonatsea@duck.com"]
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 20
